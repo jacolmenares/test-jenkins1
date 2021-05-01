3e052d7938bde59388022f0401ffc0ba300e59b8
Ubuntu <ubuntu@ip-172-31-18-107.us-east-2.compute.internal>
2021-05-01 21:24:54 +0000

    agregado de app test flask

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# save this as app.py
+from flask import Flask, escape, request
+app = Flask(__name__)
+
+
+def suma(a,b):
+    return a+b
+
+@app.route('/')
+def hello():
+    res = suma (3,2)
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}! %s' % (res)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
